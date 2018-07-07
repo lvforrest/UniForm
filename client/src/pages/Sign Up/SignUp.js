018d2c2c20711596d39795b7553e35af540697b6
Lacie Forrest <lvforrest@yahoo.com>
2018-07-07 11:38:04 -0400

    update

---
+++ client/src/pages/Sign Up/SignUp.js
@@ -0,0 +1,87 @@
+import React, { Component } from "react";
+import API from "../../utils/API";
+import { Col, Row, Container } from "../../components/Grid";
+import { Input, FormBtn } from "../../components/InputField";
+import Jumbotron from "../../components/Jumbotron";
+
+class SignUp extends Component {
+  state = {
+    firstName: "", 
+    lastName: "",
+    email: "",
+    password:"",
+    name: "signup"
+  }
+
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    if (this.state.email && this.state.firstName ) {
+      API.saveUser({
+        firstName: this.state.firstName,
+        lastName:this.state.lastName,
+        email: this.state.email,
+        password: this.state.password,
+      })
+        .then(alert("User Saved"))
+        .catch(err => console.log(err));
+    }
+  };
+  render() {
+  return(
+  <div>
+  <Jumbotron name = {this.state.name} children = {this.state.name} />
+  <Container fluid>
+    <Row>
+      <Col size="md-12">
+       
+          <form>
+            <center>
+    
+              <Input
+                value={this.state.firstName}
+                onChange={this.handleInputChange}
+                name="firstName"
+                placeholder="First Name(required)"
+              />
+              <Input
+                value={this.state.lastName}
+                onChange={this.handleInputChange}
+                name="last name"
+                placeholder= "Last name (required)"
+              />
+              <Input
+                value={this.state.email}
+                onChange={this.handleInputChange}
+                name="email"
+                placeholder="email (required)"
+              />
+              <Input
+                value={this.state.password}
+                onChange={this.handleInputChange}
+                name="password"
+                placeholder="password (required)"
+              />
+              <center><FormBtn
+                disabled={!(this.state.email && this.state.firstName)}
+                onClick={this.handleFormSubmit}
+              >
+                Submit  
+              </FormBtn></center>
+              </center>
+            </form>
+          
+        
+      </Col>
+    </Row>
+  </Container>
+  </div>
+)}
+}
+export default SignUp;
